7143451a846911f01fdb117e5854e2b589fa1f70
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-12-26 21:08:56 +0300

    reading and Writing

--- src/main/java/com/slippery/Main.java
+++ src/main/java/com/slippery/Main.java
@@ -1,14 +1,62 @@
 package com.slippery;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main{
-    public static void main(String [] args) throws IOException {
-        File texts =new File("/home/slippery/Documents/textsFile.txt");
-        if(texts.createNewFile()){
-            System.out.println("new file created at "+texts.toString().substring(0,texts.toString().length() -14) );
-        }else{
-            System.out.println("file not created");
+    public static void main(String[] args) throws IOException {
+//        creation
+//        String fileCreation =createNewFile("/home/slippery/Documents/names.pdf");
+//        System.out.println(fileCreation);
+//        writing
+        String fileWriting =writeToFile("/home/slippery/Documents/names.pdf");
+        System.out.println(fileWriting);
+
+//reading
+        String fileReader =readFromFile("/home/slippery/Documents/names.pdf");
+        System.out.println(fileReader);
+    }
+    public static String createNewFile(String name) throws IOException{
+        try{
+            File namesTxt =new File(name);
+            if(namesTxt.createNewFile()){
+                return "new file created";
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+            return "error "+e.getMessage();
         }
+        return null;
+    }
+    public static String readFromFile(String name) throws FileNotFoundException {
+        File file =new File(name);
+        Scanner scanner =new Scanner(file);
+        if(scanner.hasNext()){
+            return scanner.next();
+        }else{
+            throw new RuntimeException("file is empty");
+        }
+
+    }
+    public static String writeToFile(String name) throws IOException{
+        try{
+            FileWriter writer =new FileWriter(name);
+            StringBuilder messages = new StringBuilder();
+            messages.append("new file \n");
+            messages.append("new file 1 \n");
+            messages.append("new file 2\n");
+            messages.append("new file 4\n");
+            messages.append("new file 6 \n");
+
+            writer.write(messages.toString());
+            writer.close();
+            return "message written to file";
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
---
+++ src/main/java/com/slippery/creation/Creation.java
@@ -0,0 +1,14 @@
+package com.slippery.creation;
+
+import java.io.*;
+
+public class Creation {
+    public static void main(String [] args) throws IOException {
+        File texts =new File("/home/slippery/Documents/textsFile.txt");
+        if(texts.createNewFile()){
+            System.out.println("new file created at "+texts.toString().substring(0,texts.toString().length() -14) );
+        }else{
+            System.out.println("file not created");
+        }
+    }
+}
